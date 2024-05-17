8221aac8fbad6cf4564fdcc14b15edfe36bed275
cosmos-369 <cosmos333999@gmail.com>
2024-05-17 21:25:48 +0530

    refactor : add gracful shutdown to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"whtester/server"
 )
 
 func main() {
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool)
 
 	if len(os.Args) > 3 {
 		fmt.Println(os.Args)
@@ -20,5 +27,25 @@ func main() {
 
 	clientsManager := server.NewManager()
 	mux := server.NewWebHookHandler(clientsManager, domain)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), mux))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
+
+	// start server
+	go func() {
+		srv.ListenAndServe()
+	}()
+
+	// gracefull shutdown server
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println("Shutting Down Server")
+		fmt.Println(sig)
+		srv.Shutdown(context.Background())
+		done <- true
+	}()
+
+	<-done
 }
